6aa8265fab28d5c2ca88b46757f52e80783c16d9
tylekmj-ops <tylekmj@gmail.com>
2025-10-01 11:41:40 -0400

    Problems 1-5 completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 //Code your solutions in this file
+const fiveToOneHundred = () => {
+    for (let i = 5; i <= 100; i++){
+        console.log(i)
+    }
+}
+
+const multiplesOfThree = () => {
+    for (let i = 3; i <= 100; i += 3){
+        console.log(i)
+    }
+}
+
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 3; i <= 100; i++){
+        if (i % 3 == 0 || i % 5 == 0) {
+            console.log(i)
+        }
+    }
+}
+
+const untilNum = (num) => {
+    for (let i = 1; i <= num; i++) {
+        console.log(i)
+    }
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2
+}
